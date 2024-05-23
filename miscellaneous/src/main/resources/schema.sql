4491e02bfaac2ad6fcff160a79bfbe4be74da3dc
Benjamin Guegan <bguegan@outlook.com>
2024-05-23 18:35:38 -0400

    add basic-authentication

--- miscellaneous/src/main/resources/schema.sql
+++ miscellaneous/src/main/resources/schema.sql
@@ -1,4 +1,5 @@
 drop table if exists spaces;
+
 create table spaces(
   space_id int primary key, 
   name varchar(255) not null, 
@@ -9,7 +10,7 @@ create sequence space_id_seq;
 drop table if exists messages;
 create table messages(
   space_id int not null references spaces(space_id), 
-  msg_id int primary key
+  msg_id int primary key,
   author varchar(30) not null, 
   msg_time timestamp not null default current_timestamp, 
   msg_text varchar(1024) not null
@@ -18,6 +19,6 @@ create sequence msg_id_seq;
 create index msg_timestamp_idx on messages(msg_time);
 create unique index space_name_idx on spaces(name);
 
-create user api_user password "password";
+create user api_user PASSWORD 'password';
 grant select, insert on spaces, messages to api_user;
 grant delete on messages to api_user;
